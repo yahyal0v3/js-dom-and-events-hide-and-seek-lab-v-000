8febb5bfafa3834715e9b4bf89c6106137f784d1
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-07 01:21:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  document.querySelector('div#grand-node div div div div')
+  return document.querySelector('div#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
 }
